a91e48e751d289bc0e9d1b8eeb4430e08d6ebb40
Parijat Biswas <parijatb32@gmail.com>
2026-02-05 15:45:18 +0530

    feat: setup core application layout and routing

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Services from './components/services/Services';
+import Resume from './components/resume/Resume';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import useLocalStorage from 'use-local-storage'
+
+
+function App() {
+    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+    const switchTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+    }
+
+    return (
+        <div className="app" data-theme={theme}>
+            <Sidebar theme={theme} switchTheme={switchTheme} />
+            <main className='main'>
+                <Home />
+                <About />
+                <Services />
+                <Resume />
+                <Portfolio />
+                <Contact theme={theme} />
+                <Footer />
+            </main>
+        </div>
+    );
+}
+
+export default App;
